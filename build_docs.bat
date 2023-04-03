907fc162f4461e5ea0c63f4ee05d347aa10e32fe
Andreas Krauss <128487507+AndreasKrauss42@users.noreply.github.com>
2023-04-03 15:22:54 +0200

    build: integrate structurizr (#3)

--- build_docs.bat
+++ build_docs.bat
@@ -1,1 +1,5 @@
-sphinx-build -b html docs _build
+echo "Generate views using structurizr"
+:: ATTENTION: do not provide more than one workspace
+call structurizr export -workspace docs/_assets/structurizr/ -format plantuml -output docs/_assets/structurizr/
+echo "Start building documentation using Sphinx"
+call sphinx-build -b html docs _build
