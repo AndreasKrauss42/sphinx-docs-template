048b6eed7929550edd4d2262836ae8a34311130d
Andreas Krauss <andreas.krauss@vector.com>
2023-04-03 13:16:55 +0200

    build: add support for structurizr

--- build_docs.bat
+++ build_docs.bat
@@ -1,1 +1,5 @@
-sphinx-build -b html docs _build
+echo "Generate views using structurizr"
+:: ATTENTION: do not provide more than one workspace
+call structurizr export -workspace docs/_assets/structurizr/ -format plantuml
+echo "Start building documentation using Sphinx"
+call sphinx-build -b html docs _build
--- build_docs.sh
+++ build_docs.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
+echo "Generate views using structurizr"
+# ATTENTION: do not provide more than one workspace
+structurizr export -workspace docs/_assets/structurizr/ -format plantuml
+echo "Start building documentation using Sphinx"
 sphinx-build -b html docs _build    
