3224f5c3e991532497ddaf3d7efc515d6c7bf50f
Andreas Krauss <andreas.krauss@vector.com>
2023-09-07 12:55:18 +0200

    build: use --mount and improve location handling

--- build_docs.sh
+++ build_docs.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 echo "Start building documentation using Sphinx"
 PROJECT_ROOT="$(if [[ "$OSTYPE" == "msys" ]]; then cygpath -w `pwd`; else pwd; fi)"
+mkdir -p ${PROJECT_ROOT}/_build
 docker start sphinx-gxc ||\
     (echo "Create container..." &&\
     docker run -d -it \
         --name=sphinx-gxc \
         -p 31442:8000 \
-        -v ${PROJECT_ROOT}/docs:/docs/src \
-        -v ${PROJECT_ROOT}/_build:/docs/_build \
+        --mount type=bind,source="${PROJECT_ROOT}"/docs,target=/home/sphinx/docs/src \
+        --mount type=bind,source="${PROJECT_ROOT}"/docs,target=/home/sphinx/docs/_build \
         sphinx-gx:latest \
         --watch)
 echo "Access the documentation under http://localhost:31442/index.html"
--- docker/lib/entrypoint.sh
+++ docker/lib/entrypoint.sh
@@ -7,18 +7,18 @@ CWD=$(pwd)
 # if args contains --watch, run onetime mode
 if echo "$ARGS" | grep -q -e '-h' -e '--watch'; then
     echo "Run in watch mode..."
-    structurizr export -workspace /docs/src/assets/structurizr/ -format plantuml -output $CWD/docs/assets/structurizr/
+    structurizr export -workspace $CWD/src/assets/structurizr/ -format plantuml -output $CWD/src/assets/structurizr/
     bash -c "watchmedo shell-command \
                 --patterns='*.dsl' \
                 --ignore-pattern='/docs/_build/*' \
                 --recursive \
-                --command='structurizr export -workspace ${CWD}/docs/assets/structurizr -format plantuml -output ${CWD}/docs/assets/structurizr/' \
+                --command='structurizr export -workspace ${CWD}/src/assets/structurizr -format plantuml -output ${CWD}/src/assets/structurizr/' \
                 --debug-force-polling \
                 /docs/src/assets/structurizr &"
-	sphinx-autobuild -a -b html --host 0.0.0.0 --port 8000 docs _build/html
+	sphinx-autobuild -a -b html --host 0.0.0.0 --port 8000 src _build/html
 else
     echo "Generate views of C4 model..."
-    structurizr export -workspace $CWD/docs/assets/structurizr -format plantuml -output $CWD/docs/assets/structurizr/
+    structurizr export -workspace $CWD/src/assets/structurizr -format plantuml -output $CWD/src/assets/structurizr/
     echo "Start Sphinx-Generator..."
-	sphinx-build -b html docs _build -a
+	sphinx-build -b html $CWD/src $CWD/_build -a
 fi
