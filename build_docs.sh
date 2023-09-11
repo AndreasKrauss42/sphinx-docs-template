38196c8e063e64c53d1fa7c0cfddd8575416f266
Andreas Krauss <andreas.krauss@vector.com>
2023-09-11 07:30:45 +0200

    build: simplify paths

--- build_docs.sh
+++ build_docs.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 echo "Start building documentation using Sphinx"
 PROJECT_ROOT="$(if [[ "$OSTYPE" == "msys" ]]; then cygpath -w `pwd`; else pwd; fi)"
-mkdir -p ${PROJECT_ROOT}/_build
+CONTAINER_ROOT=/home/sphinx/docs
+HOST_SRC=docs
+CONT_SRC=src
+BUILD=_build
+mkdir -p ${PROJECT_ROOT}/${BUILD}
 docker start sphinx-gxc ||\
     (echo "Create container..." &&\
     docker run -d -it \
         --name=sphinx-gxc \
         -p 31442:8000 \
-        --mount type=bind,source="${PROJECT_ROOT}"/docs,target=/home/sphinx/docs/src \
-        --mount type=bind,source="${PROJECT_ROOT}"/docs,target=/home/sphinx/docs/_build \
+        --mount type=bind,source="${PROJECT_ROOT}"/"${HOST_SRC}",target="${CONTAINER_ROOT}"/"${CONTAINER_SRC}" \
+        --mount type=bind,source="${PROJECT_ROOT}"/"${BUILD}",target="${CONTAINER_ROOT}"/"${BUILD}" \
         sphinx-gx:latest \
         --watch)
 echo "Access the documentation under http://localhost:31442/index.html"
