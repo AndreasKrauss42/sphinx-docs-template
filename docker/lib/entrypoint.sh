63e467ad4632a727953fb95f54d5af93db645148
Andreas Krauss <andreas.krauss@vector.com>
2023-09-12 10:57:35 +0200

    chore: increase robustness against absence of dsl files

--- docker/lib/entrypoint.sh
+++ docker/lib/entrypoint.sh
@@ -5,21 +5,33 @@ echo "Arguments: $ARGS"
 CWD=$(pwd)
 STRUCTURIZR_PATH=$CWD/src/assets/structurizr
 
+check_and_render_C4_dsl () {
+	echo "Testing for a C4 model resource."
+	DSL_FILES=$(find . -type f -name "*.dsl" | wc -l )
+	if [ $DSL_FILES -gt 1 ]; then
+  		echo "DSL file found. Processing file with 'structurizr'.";
+    	echo "Generate views of C4 model..."
+		structurizr export -workspace $STRUCTURIZR_PATH -format plantuml -output $STRUCTURIZR_PATH/
+	else
+  		echo "No DSL file found."
+	fi
+}
+
 # if args contains --watch, run onetime mode
 if echo "$ARGS" | grep -q -e '-h' -e '--watch'; then
     echo "Run in watch mode..."
-    structurizr export -workspace $STRUCTURIZR_PATH -format plantuml -output $STRUCTURIZR_PATH
-    bash -c "watchmedo shell-command \
-                --patterns='*.dsl' \
-                --ignore-pattern='${CWD}/_build/*' \
-                --recursive \
-                --command='structurizr export -workspace ${STRUCTURIZR_PATH} -format plantuml ${STRUCTURIZR_PATH}' \
-                --debug-force-polling \
-                ${STRUCTURIZR_PATH} &"
+	echo "Testing for a C4 model resource."
+	check_and_render_C4_dsl
+	bash -c "watchmedo shell-command \
+			--patterns='*.dsl' \
+			--ignore-pattern='${CWD}/_build/*' \
+			--recursive \
+			--command='structurizr export -workspace ${STRUCTURIZR_PATH} -format plantuml -output ${STRUCTURIZR_PATH}/' \
+			--debug-force-polling \
+			${STRUCTURIZR_PATH} &"
 	sphinx-autobuild -a -b html --host 0.0.0.0 --port 8000 src _build/html
 else
-    echo "Generate views of C4 model..."
-    structurizr export -workspace $STRUCTURIZR_PATH -format plantuml -output $STRUCTURIZR_PATH
+	check_and_render_C4_dsl
     echo "Start Sphinx-Generator..."
 	sphinx-build -a -b html $CWD/src $CWD/_build
 fi
